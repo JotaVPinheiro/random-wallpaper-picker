be558a43366d57d73a25f3be48457e24c5b1dc95
JVJVJVJVJVJV <pinheiro.v@escolar.ifrn.edu.br>
2022-03-28 19:15:12 -0300

    get random posts from r/wallpaper or r/wallpapers

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ import fetch from "node-fetch";
 import { PowerShell } from "node-powershell";
 import ImageDownloader from "image-downloader";
 
-let redditUrl = "https://www.reddit.com/r/wallpapers/.json"
 let filePath = process.cwd() + "/img.png"
+const subs = ['wallpaper', 'wallpapers']
+
+let redditUrl = RedditURLTo(subs[Math.floor(Math.random() * subs.length)])
+console.log(redditUrl)
 
 fetch(redditUrl)
 .then(response => response.json())
 .then(body => {
-    for(let i = 0; i < body.data.children.length; i++) {
-        if(body.data.children[i].data.post_hint == 'image') {
-            DownloadImage(body.data.children[i].data.url_overridden_by_dest)
-            break
-        }
-    }
+    let validPosts = []
+
+    body.data.children.forEach((post) => {
+        if(post.data.post_hint == 'image')
+            validPosts.push(post.data.url_overridden_by_dest)
+    })
+
+    DownloadImage(validPosts[Math.floor(Math.random() * validPosts.length)])
 })
 
 function DownloadImage(url) {
@@ -24,4 +29,8 @@ function DownloadImage(url) {
     .then(() => {
         PowerShell.$`./SetWall.ps1`
     })
+}
+
+function RedditURLTo(sub) {
+    return `https://www.reddit.com/r/${sub}/.json`
 }

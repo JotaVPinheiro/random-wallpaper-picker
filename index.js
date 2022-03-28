58802851d246c362c5f8b655d6ff2671bf79a124
JVJVJVJVJVJV <pinheiro.v@escolar.ifrn.edu.br>
2022-03-28 19:23:17 -0300

    comments added

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
+//Import of modules
 import fetch from "node-fetch";
 import { PowerShell } from "node-powershell";
 import ImageDownloader from "image-downloader";
 
+//Path to the cache image
 let filePath = process.cwd() + "/img.png"
+
+//List of subreddits to search
 const subs = ['wallpaper', 'wallpapers']
 
+//Picking the url to a random sub on the list
 let redditUrl = RedditURLTo(subs[Math.floor(Math.random() * subs.length)])
-console.log(redditUrl)
 
+//Magic
 fetch(redditUrl)
 .then(response => response.json())
 .then(body => {
-    let validPosts = []
+    //List of urls to the wallpapers
+    let imagesUrls = []
 
+    //Add to the array the url to the image of each post that is an actual image
     body.data.children.forEach((post) => {
         if(post.data.post_hint == 'image')
-            validPosts.push(post.data.url_overridden_by_dest)
+            imagesUrls.push(post.data.url_overridden_by_dest)
     })
 
-    DownloadImage(validPosts[Math.floor(Math.random() * validPosts.length)])
+    //Download the chosen wallpaper
+    DownloadImage(imagesUrls[Math.floor(Math.random() * imagesUrls.length)])
 })
 
+//Function that download the image from the chosen post
 function DownloadImage(url) {
     ImageDownloader.image({
         url: url,
@@ -31,6 +40,7 @@ function DownloadImage(url) {
     })
 }
 
+//Function that returns the url to the chosen sub
 function RedditURLTo(sub) {
     return `https://www.reddit.com/r/${sub}/.json`
 }

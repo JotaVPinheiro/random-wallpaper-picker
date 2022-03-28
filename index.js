2c9c1ab339e5d34e3a645a4c48aefbfeac0bdf58
JVJVJVJVJVJV <pinheiro.v@escolar.ifrn.edu.br>
2022-03-28 19:53:26 -0300

    debuggin added

--- index.js
+++ index.js
@@ -3,39 +3,52 @@ import fetch from "node-fetch";
 import { PowerShell } from "node-powershell";
 import ImageDownloader from "image-downloader";
 
+console.log('Starting...')
+
 //Path to the cache image
 let filePath = process.cwd() + "/img.png"
 
 //List of subreddits to search
 const subs = ['wallpaper', 'wallpapers']
 
-//Picking the url to a random sub on the list
-let redditUrl = RedditURLTo(subs[Math.floor(Math.random() * subs.length)])
+//Random sub chosen from the list
+let chosenSub = subs[Math.floor(Math.random() * subs.length)]
+console.log(`Chosen sub: r/${chosenSub}`)
+
+//Picking the url to the chosen sub
+let redditUrl = RedditURLTo(chosenSub)
 
 //Magic
 fetch(redditUrl)
 .then(response => response.json())
 .then(body => {
-    //List of urls to the wallpapers
-    let imagesUrls = []
+    //List of post with images
+    let postsURLs = []
 
-    //Add to the array the url to the image of each post that is an actual image
+    //Add to the array the url to the posts with images
     body.data.children.forEach((post) => {
         if(post.data.post_hint == 'image')
-            imagesUrls.push(post.data.url_overridden_by_dest)
+            postsURLs.push(post)
     })
 
+    //Random post chosen from the list
+    let chosenPost = postsURLs[Math.floor(Math.random() * postsURLs.length)]
+    console.log(`Post URL: https://www.reddit.com/${chosenPost.data.permalink}`)
+    console.log(`Image URL: ${chosenPost.data.url_overridden_by_dest}`)
+
     //Download the chosen wallpaper
-    DownloadImage(imagesUrls[Math.floor(Math.random() * imagesUrls.length)])
+    DownloadImage(chosenPost.data.url_overridden_by_dest)
 })
 
 //Function that download the image from the chosen post
 function DownloadImage(url) {
+    console.log('Downloading image...')
     ImageDownloader.image({
         url: url,
         dest: filePath
     })
     .then(() => {
+        console.log('Setting wallpaper...')
         PowerShell.$`./SetWall.ps1`
     })
 }
